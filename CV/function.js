0c4735dfeb46b1b4a91bd02a2e87c9a45982f92b
santichac <santichac3@gmail.com>
2023-02-28 15:26:19 -0300

    Termine el proyecto

--- CV/function.js
+++ CV/function.js
@@ -1,24 +1,9 @@
-(function(){
-    $(".programacion ul li")
-        .find(".bar")
-        .each(function(i){
-            $(this)
-            .find(".barra")
-            .delay(i*150)
-            .animate({
-                width:
-                $(this)
-                    .parents()
-                    .attr("data-percent") + "%"
-                },
-                1000,
-                "linear",
-                function(){
-                    return $(this).css({
-                        "transition-duration": ".5s"
-                    });
-                }
-            );
-        });
-    
-}.call(this));
+document.getElementById("oscuro").onclick = function(){
+    document.body.style.backgroundColor ="#000000";
+    console.log("Cambio a negro");  
+}
+
+document.getElementById("claro").onclick = function(){
+    document.body.style.backgroundColor = "#808080";
+    console.log("Volvio a gris");  
+}
